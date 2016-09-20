b71aebc2a28bc4d88957ed8804cfc8bf44173801
Chen <jchen@laterooms.com>
2016-09-20 14:30:14 +0100

    Missed a renaming.

--- tests/UnitTests/HotelCSVDataFeedTests.cs
+++ tests/UnitTests/HotelCSVDataFeedTests.cs
@@ -82,7 +82,7 @@ namespace UnitTests
         }
 
         [Test]
-        public void HotelCSVDataFeed_can_make_notification()
+        public void HotelCSVDataFeed_can_make_notification_via_Twitter()
         {
             // Arrange
             var notifier = MockRepository.GenerateMock<INotifier>();
