a65b734bda529eccd6067f2a54dc87b1965bd7b6
Chen <jchen@laterooms.com>
2016-09-21 09:52:07 +0100

    Introduced a new class RateCSVDataFeed derived from abstract base class with different notifier for implementation.

---
+++ src/PatternLib/RateCSVDataFeed.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternLib
+{
+    public class RateCSVDataFeed : BaseDataFeed
+    {
+        private INotifier _notifier = new TwitterNotifier();
+
+        public RateCSVDataFeed(INotifier notifier) : base(notifier)
+        {
+            _notifier = notifier;
+        }
+
+        public override void Notify()
+        {
+            _notifier.UpdateObservers(_affiliates);
+        }
+    }
+}
